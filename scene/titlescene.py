9651e9a89792bc221b3d4b0862f1b9cb18ae1d43
kxd893 <kxd893@student.bham.ac.uk>
2022-07-30 14:34:01 +0100

    added high score display on title screen

--- scene/titlescene.py
+++ scene/titlescene.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 from scene.scene import Scene
 from scene.levelscene import LevelScene
 from ui.button import Button
@@ -8,19 +9,26 @@ class TitleScene(Scene):
     def __init__(self):
         self.logo = pygame.image.load(r'ui/ui-graphics/logo.png').convert()
         self.start = Button("start", self.switch_to_level, 50, 150, 150, 20)
+        self.font = pygame.font.SysFont("Calibri", 14)
+        self.highscore = 0
         self.next = self
 
     def buttons(self):
         return [self.start]
 
+    def highscore_text(self):
+        return self.font.render(f"High score: {self.highscore}", 0, (255, 255, 255))
+
     def update(self, delta):
-        pass
+        self.highscore = json.load(open('config.json'))['high-score']
 
     def process_input(self, events):
         for button in self.buttons():
             button.process_input(events)
 
     def render(self, screen):
+        hs = self.highscore_text()
+        screen.blit(hs, hs.get_rect(center=(50, 125)))
         screen.blit(self.logo, self.logo.get_rect(topleft=(50, 20)))
         for button in self.buttons():
             button.render(screen)
